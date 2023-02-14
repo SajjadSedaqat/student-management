9d6ad5a486ae595453b252d62c2ccd9e81968c83
SajjadSedaqat <Sajjad.Sedaqat@Gmail.com>
2023-02-14 19:24:41 +0330

    Add simple backend

---
+++ src/main/java/com/sajjad/studentmanagement/student/Gender.java
@@ -0,0 +1,7 @@
+package com.sajjad.studentmanagement.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/sajjad/studentmanagement/student/Student.java
@@ -0,0 +1,19 @@
+package com.sajjad.studentmanagement.student;
+
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+
+
+}
